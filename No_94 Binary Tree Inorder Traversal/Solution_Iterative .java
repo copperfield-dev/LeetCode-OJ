0259cab1036ab2798f04098c517c03be8101e368
Copperfield <xwhfcenter@gmail.com>
2015-03-04 22:42:16 +0800

    提交了第94题的迭代解答

---
+++ No_94 Binary Tree Inorder Traversal/Solution_Iterative .java
@@ -0,0 +1,28 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<Integer>();
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            else {
+                root = stack.pop();
+                answer.add(root.val);
+                root = root.right;
+            }
+        }
+
+        return answer;
+    }
+}
