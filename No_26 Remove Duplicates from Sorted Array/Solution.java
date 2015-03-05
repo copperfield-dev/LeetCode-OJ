82f01e0fa9e10f8259ac71589da3a2fea2b69189
Copperfield <xwhfcenter@gmail.com>
2015-03-05 19:44:01 +0800

    提交了第26题的解答

---
+++ No_26 Remove Duplicates from Sorted Array/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        if (A.length == 0)
+            return 0;
+        int cur = A[0];
+        int i = 0;
+        int length = 1;
+
+        while (i < A.length - 1) {
+            while (A[++i] == cur);
+            cur = A[i];
+            A[length] = A[i]; 
+            length++;          
+        } 
+        return length;
+    }
+}
