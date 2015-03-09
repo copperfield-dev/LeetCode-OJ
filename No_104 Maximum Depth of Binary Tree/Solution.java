14587e631c43926765127f37b8311eac9f3e363b
Copperfield <xwhfcenter@gmail.com>
2015-03-09 23:32:11 +0800

    修改了理解上的错误，求深度使用先序遍历

--- No_104 Maximum Depth of Binary Tree/Solution.java
+++ No_104 Maximum Depth of Binary Tree/Solution.java
@@ -9,10 +9,10 @@
  */
 public class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null)
-            return 0;
-        else
+        if (root != null)
             return 1 + Math.max(maxDepth(root.left),
                 maxDepth(root.right));
+        else
+            return 0;
     }
 }
