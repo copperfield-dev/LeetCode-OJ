0fd3a16866e461725689257ec780c2a9aca4687e
Copperfield <xwhfcenter@gmail.com>
2015-03-11 17:01:36 +0800

    提交了第112题的解答

---
+++ No_112 Path Sum/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+            
+        /* 对叶子节点进行比较 */
+        if (root.left == null && root.right == null) {
+            if (root.val == sum)
+                return true;
+            else 
+                return false;
+        }
+        else
+            return (hasPathSum(root.left, sum - root.val) 
+                || hasPathSum(root.right, sum - root.val));
+    }
+}
