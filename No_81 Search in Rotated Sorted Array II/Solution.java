851c6e260677b21200f55031d4a8fb290bf2e24b
Copperfield <xwhfcenter@gmail.com>
2015-03-23 17:00:38 +0800

    修改了第81题的代码

--- No_81 Search in Rotated Sorted Array II/Solution.java
+++ No_81 Search in Rotated Sorted Array II/Solution.java
@@ -8,24 +8,25 @@ public class Solution {
 
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if (target < A[mid]) {
-                if (target >= A[lo]) 
-                    hi = mid - 1;
-                else if (mid != A.length - 1 && A[mid + 1] <= A[mid])
-                    lo = mid + 1;
-                else if (mid == A.length - 1)
-                    return false;
-            }
-            else if (target > A[mid]) {
-                if (target <= A[hi])
-                    lo = mid + 1;
-                else if (mid != 0 && A[mid - 1] >= A[mid])
-                    hi = mid - 1;
-                else if (mid == 0)
-                    return false;
-            }
-            else
+
+            if (A[mid] == target)
                 return true;
+            if (A[lo] < A[mid]) {           //left subsequence sorted
+                if (A[lo] <= target && target < A[mid])
+                    hi = mid - 1;
+                else
+                    lo = mid + 1;
+            }
+            else if (A[mid] < A[hi]) {      //right subsequence sorted
+                if (A[hi] >= target && target > A[mid])
+                    lo = mid + 1;
+                else
+                    hi = mid - 1;
+            }
+            else if (A[lo] == A[mid])
+                lo++;
+            else if (A[mid] == A[hi])
+                hi--;
         }
         return false;
     }
