c9de8ccba3ad5abe0ec436c1bff5345bda141ff5
Copperfield <xwhfcenter@gmail.com>
2015-04-02 10:13:30 +0800

    修改了第189题的解答

--- No_189 Rotate Array/Solution.java
+++ No_189 Rotate Array/Solution.java
@@ -6,9 +6,8 @@ public class Solution {
         for (int i = 0; i < k; i++) {
             temp = nums[nums.length - k + i];    //第i个开始循环的数
             int p = nums.length - k + i;
-            for (int j = 0; j < nums.length - k; j++) {
+            for (int j = 0; j < nums.length - k; j++) 
                 nums[p] = nums[--p];
-            }
             nums[p] = temp;
         }
     }
